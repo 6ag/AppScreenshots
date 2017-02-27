302bc0a29241d65e13e7c5bb948d8d052f6ccc4f
zhoujianfeng <admin@6ag.cn>
2017-02-27 17:04:38 +0800

    修改相册动画

--- AppScreenshots/Classes/Module/Home/Transition/JFPhotoPopoverDismissAnimation.swift
+++ AppScreenshots/Classes/Module/Home/Transition/JFPhotoPopoverDismissAnimation.swift
@@ -20,14 +20,11 @@ class JFPhotoPopoverDismissAnimation: NSObject, UIViewControllerAnimatedTransiti
         
         NotificationCenter.default.post(name: Notification.Name(rawValue: AlbumListViewControllerWillDismiss), object: nil)
         
-        // 获取到modal出来的控制器的view
         let fromView = transitionContext.view(forKey: UITransitionContextViewKey.from)!
-        
-        // 动画缩放modal出来的控制器的view到看不到
-        UIView.animate(withDuration: transitionDuration(using: nil), delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 1, options: UIViewAnimationOptions(rawValue: 0), animations: { () -> Void in
-            fromView.alpha = 0
-            }, completion: { (_) -> Void in
-                transitionContext.completeTransition(true)
-        })
+        UIView.animate(withDuration: transitionDuration(using: nil), animations: {
+            fromView.alpha = 0.0
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
     }
 }
--- AppScreenshots/Classes/Module/Home/Transition/JFPhotoPopoverModalAnimation.swift
+++ AppScreenshots/Classes/Module/Home/Transition/JFPhotoPopoverModalAnimation.swift
@@ -18,20 +18,14 @@ class JFPhotoPopoverModalAnimation: NSObject, UIViewControllerAnimatedTransition
     // modal动画
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         
-        // 获取到需要modal的控制器的view
         let toView = transitionContext.view(forKey: UITransitionContextViewKey.to)!
-        
-        // 将需要modal的控制器的view添加到容器视图
         transitionContext.containerView.addSubview(toView)
+        toView.alpha = 0.0
         
-        toView.transform = CGAffineTransform(scaleX: 1, y: 0)
-        toView.layer.anchorPoint = CGPoint(x: 0.5, y: 0.0)
-        
-        // 动画缩放modal的控制器的view到正常大小
-        UIView.animate(withDuration: transitionDuration(using: nil), delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 5, options: UIViewAnimationOptions(rawValue: 0), animations: { () -> Void in
-            toView.transform = CGAffineTransform.identity
-            }, completion: { (_) -> Void in
-                transitionContext.completeTransition(true)
-        })
+        UIView.animate(withDuration: transitionDuration(using: nil), animations: { 
+            toView.alpha = 1.0
+        }) { (_) in
+            transitionContext.completeTransition(true)
+        }
     }
 }
